34eceaa8a9c5e8cf382cb766522f3943b448bf95
Ponestina <112325545+Ponestina@users.noreply.github.com>
2022-10-26 16:48:11 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -61,7 +61,7 @@ window.addEventListener('DOMContentLoaded', event => {
 var textWrapper = document.querySelector('.ml13');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: 4})
+anime.timeline({loop: 2})
   .add({
     targets: '.ml13 .letter',
     translateY: [100,0],
@@ -81,7 +81,7 @@ anime.timeline({loop: 4})
 var textWrapper = document.querySelector('.ml10 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: 4})
+anime.timeline({loop: 2})
   .add({
     targets: '.ml10 .letter',
     rotateY: [-90, 0],
@@ -95,7 +95,7 @@ anime.timeline({loop: 4})
 var textWrapper = document.querySelector('.ml11 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: 4})
+anime.timeline({loop: 2})
   .add({
     targets: '.ml11 .line',
     scaleY: [0,1],
@@ -128,7 +128,7 @@ anime.timeline({loop: 4})
 
 
 
-anime.timeline({loop: 3})
+anime.timeline({loop: 2})
   .add({
     targets: '.ml5 .line',
     opacity: [0.5,1],
